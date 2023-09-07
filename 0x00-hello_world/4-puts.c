9f311d913a921fde0fe10988582e64d289d3426a
Omosalewa-Akinsete <shaleycoral@gmail.com>
2023-09-07 11:20:58 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main -it prints out output using put
+ * main - Entry point
  *
- * Return: always (0)
+ * Return: Always 0 (Success)
  *
  */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
+	puts("\""Programming is like building a multilingual puzzle);
 	return (0);
 }
