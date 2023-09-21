d50b79069b946121ac84dca67bb0fb8a59957b41
Omosalewa-Akinsete <shaleycoral@gmail.com>
2023-09-21 13:22:20 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,17 +8,17 @@
 
 char *leet(char *s)
 {
-	int i, j;
-	char subs[] = "aAeEoOtTlL";
-	char le[] = "43071";
+int i, j;
+char subs[] = "aAeEoOtTlL";
+char le[] = "43071";
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		for (j = 0; subs[j] != '\0'; j++)
-			if (s[i] == subs[j])
-			{
-				s[i] = le[j / 2];
-			}
-	}
-	return (s);
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; subs[j] != '\0'; j++)
+if (s[i] == subs[j])
+{
+s[i] = le[j / 2];
+}
+}
+return (s);
 }
