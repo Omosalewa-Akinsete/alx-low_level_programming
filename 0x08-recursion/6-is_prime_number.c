4da4ab2c82dabf4d25d54097966eb64df95e111e
Omosalewa-Akinsete <shaleycoral@gmail.com>
2023-09-28 14:18:24 -0700

    Inception Is it possible?

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * prime_a -  Returns 1 if the input integer is a prime number, otherwise return 0.
+ * @a: The string to be printed.
+ * @b: The string to be printed.
+ * Return: 0
+ */
+int prime_a(int a, int b)
+{
+	if (a <= 1 || (a != b && a % b == 0))
+	{
+		return (0);
+	}
+	else if (a == b)
+	{
+		return (1);
+	}
+	return (prime_a(a, b + 1));
+}
+
+/**
+ * is_prime_number -  Returns 1 if the input integer is a prime number, otherwise return 0.
+ * @n: The sring to be printed.
+ * Return: 0
+ */
+int is_prime_number(int n)
+{
+	return (prime_a(n, 2));
+}
