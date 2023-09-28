53a652217365bd7f8084b3c5ba392d7b0e90f943
Omosalewa-Akinsete <shaleycoral@gmail.com>
2023-09-28 10:43:10 -0700

    Dreams feel real but in reality it can feel strange

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,3 +1,14 @@
 #include "main.h"
 /**
- * _strlen_recursion - Main Entry
+ * _strlen_recursion - Returns the length of a string.
+ * @s: The string to be printed.
+ * Return: 0
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	return (0);
+}
