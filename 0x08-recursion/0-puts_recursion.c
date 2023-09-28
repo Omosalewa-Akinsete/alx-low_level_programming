f49185085aa12c58323fd30c155af0c6b1682cd1
Omosalewa-Akinsete <shaleycoral@gmail.com>
2023-09-28 06:41:44 -0700

    She locked away a secret deep inside herself something she once knew to be true but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,19 @@
 #include "main.h"
 /**
- * _puts_recursion - Main Entry
- * @s: input
+ * _puts_recursion - print a string
+ * @s: string
+ * Return: void
  */
-void _puts_recursion(char *s);
+void _puts_recursion(char *s)
 {
-	if (*(s) != '\0')
+	if (*s)
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
-	if (*(s) == '\0')
+	else
 	{
 		_putchar('\n');
 	}
+
 }
