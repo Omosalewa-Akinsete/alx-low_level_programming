f14a73f353207698aa10e7cda5c8b2c3e9beae99
Omosalewa-Akinsete <shaleycoral@gmail.com>
2023-10-24 13:27:08 -0700

    Print (safe version)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,69 +1,41 @@
 #include "lists.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
- * free_listp - frees a linked list
- * @head: head of a list.
- *
- * Return: no return.
- */
-void free_listint_t(listint_t **head)
-{
-	listint_t *temp;
-	listint_t *curr;
-
-	if (head != NULL)
-	{
-		curr = *head;
-		while ((temp = curr) != NULL)
-		{
-			curr = curr->next;
-			free(temp);
-		}
-		*head = NULL;
-	}
-}
-
-/**
- * print_listint_safe - prints a linked list.
- * @head: head of a list.
- *
- * Return: number of nodes in the list.
+ * print_listint_safe - Prints a listint_t linked list.
+ * @head: A pointer to the header of a linked list.
+ * Return: Always 0.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t nnodes = 0;
-	listint_t *hptr, *new, *add;
+	const listint_t *slower, *faster;
+	size_t count = 0;
 
-	hptr = NULL;
-	while (head != NULL)
+	slower = head;
+	faster = head;
+
+	while (slower && faster && faster->next)
 	{
-		new = malloc(sizeof(listint_t));
+		printf("%d\n", slower->n);
+		slower = slower->next;
+		faster = faster->next->next;
 
-		if (new == NULL)
-			exit(98);
-
-		new->p = (void *)head;
-		new->next = hptr;
-		hptr = new;
-
-		add = hptr;
-
-		while (add->next != NULL)
+		if (slower == faster)
 		{
-			add = add->next;
-			if (head == add->p)
-			{
-				printf("-> [%p] %d\n", (void *)head, head->n);
-				free_listint2(&hptr);
-				return (nnodes);
-			}
+			printf("-> [%p] %d\n", (void *)faster, faster->n);
+			exit(98);
 		}
-
-		printf("[%p] %d\n", (void *)head, head->n);
-		head = head->next;
-		nnodes++;
+	}
+	if (!faster)
+	{
+		return (0);
 	}
 
-	free_listint(&hptr);
-	return (nnodes);
+	while (slower != NULL)
+	{
+		printf("%d\n", slower->n);
+		slower = slower->next;
+		count++;
+	}
+	return (count);
 }
