477f898d090a1eab75b02eeb83c5e325d12e0414
Omosalewa-Akinsete <shaleycoral@gmail.com>
2023-10-24 08:23:01 -0700

    Find the loop

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,43 +1,40 @@
 #include "lists.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * delete_nodeint_at_index -  Deletes the node at a given index.
- * @head: The pointer to a pointer to the headerof the the linked list.
- * @index:The index of the node to be deleted (starting at 0).
+ * delete_nodeint_at_index -  Deletes a node at a given index in a listint_t linked list.
+ * @head: The pointer to a pointer to the head of the the linked list.
+ * @index:The index of the node to delelte.
  *
- * Returns:1 if it succeeded, -1 if it fails.
+ * Returns:1 if it successfully deleted, -1 if the node at the given indexdoes not exist.
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *temp, *dnode;
 	unsigned int i = 1;
 
-	if (head == NULL)
+	if (!head || !*head)
 		return (-1);
-
-	temp = *head;
 
 	if (index == 0)
 	{
-		*head = (*head)->next;
-		free(temp);
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+		return (1);
+	}
 
-	}
-	else
-	{
-		while (i < index)
-		{
-			temp = temp->next;
-			if (temp == NULL)
-				return (-1);
-			i++;
-		}
-		dnode = temp;
-		dnode = dnode->next;
-		temp->next = dnode->next;
-		free(dnode);
-	}
+	prev = *head;
+	for (i = 0; prev && i < index - 1; i++)
+		prev = prev->next;
+
+	if (!prev || prev->next)
+		return (-1);
+
+	temp = prev->next;
+	prev->next = temp->next;
+	free(temp);
+
 	return (1);
 }
+
---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop -  Finds the loop in a listint_t linked list.
+ * @head: A pointer to the head of the linked list.
+ *
+ * Returns: The address of the node where the loop starts, or NULL if there is no loop.
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *tortoise = head;
+	listint_t *hare = head;
+
+	while (tortoise && hare && hare->next)
+	{
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+
+		if (tortoise == hare)
+		{
+			tortoise = head;
+
+			while (tortoise != hare)
+			{
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+
+			return (tortoise);
+		}
+	}
+
+	return (NULL);
+}
