c7c9dcf678440099eb3fd29c2338c79c2e330585
Omosalewa-Akinsete <shaleycoral@gmail.com>
2023-10-30 13:42:13 -0700

    Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,11 +15,15 @@ int append_text_to_file(const char *filename, char *text_content)
 	int fd, len, bytes_written;
 
 	if (filename == NULL)
+	{
 		return (-1);
+	}
 
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
+	{
 		return (-1);
+	}
 
 	if (text_content != NULL)
 	{
