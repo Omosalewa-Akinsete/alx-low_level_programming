c722ee21f4e1b0aeb792b2b8c40ee51a176c71db
Omosalewa-Akinsete <shaleycoral@gmail.com>
2023-10-30 09:29:55 -0700

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, 0_CREAT | 0_TRUNC | 0_WRONLY, file_permissions);
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, file_permissions);
 	if (fd == -1)
 		return (-1);
 
