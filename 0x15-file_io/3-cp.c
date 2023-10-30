ed82cce9475ba675b756ea83d17befda2f35123e
Omosalewa-Akinsete <shaleycoral@gmail.com>
2023-10-30 14:48:28 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-#define BUFFER_SIZE 1024
+#define BUF_SIZE 1024
 
 /**
  * exit_with_error - Prints an error message and exit with the specified code.
@@ -26,33 +26,49 @@ int main(int argc, char *argv[])
 {
 	int fd_from, fd_to;
 	ssize_t bytes_read, bytes_written;
-	char[SIZE];
+
+	char buffer[BUF_SIZE];
 
 	if (argc != 3)
+	{
 		exit_with_error(97, "Usage: cp file_from_file_to");
+	}
 
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
+	{
 		exit_with_error(98, "Error: Can't read from file");
+	}
 
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
-		exit_with_error(99, "Error: Can't write to file");
-
-	while ((bytes_read = read(fd_from, setbuffer, BUFFER_SIZE)) > 0)
 	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written == -1)
-			exit_with_error(99, "Error: Can't write to file");
+		exit_with_error(99, "Error: Can't write to file");
 	}
 
+	while ((bytes_read = read(fd_from, buffer, BUF_SIZE)) > 0)
+	{
+		bytes_written = write(fd_to, buffer, bytes_read);
+	}
+		if (bytes_written == -1)
+		{
+			exit_with_error(99, "Error: Can't write to file");
+		}
+
+
 	if (bytes_read == -1)
+	{
 		exit_with_error(98, "Error: Can't read from file");
+	}
 
 	if (close(fd_from) == -1)
+	{
 		exit_with_error(100, "Error: Can't close fd");
+	}
 	if (close(fd_to) == -1)
+	{
 		exit_with_error(100, "Error: Can't close fd");
+	}
 
 	return (0);
 }
