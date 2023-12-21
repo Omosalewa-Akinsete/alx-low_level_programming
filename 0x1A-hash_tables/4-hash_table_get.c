4369a62c3a1bb4ed504a42f9a368f0acff16ae69
Omosalewa-Akinsete <shaleycoral@gmail.com>
2023-12-21 09:04:31 -0800

    >>> ht = {}

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Retrieves a value associated with a key
+ * @ht: The hash table to look into
+ * @key: The key to search for
+ *
+ * Return: The value associated with the element, or NULL if key is not found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *temp;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	index = key_index((unsigned char *)key, ht->size);
+
+	temp = ht->array[index];
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+			return (temp->value);
+		temp = temp->next;
+	}
+
+	return (NULL);
+}
