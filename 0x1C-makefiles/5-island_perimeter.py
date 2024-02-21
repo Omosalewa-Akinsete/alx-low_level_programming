c848d2ce626cb97fc1402bd01ce08f3bdba0fd27
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-02-21 08:22:21 -0800

    Island Perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines an island perimeter measuring function that returns the perimeter of the island described in grid."""
 
+
 def island_perimeter(grid):
     """Return the perimeter of an island.
     The grid represents water by 0 and land by 1.
